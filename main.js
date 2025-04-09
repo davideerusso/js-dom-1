80529152b308080cc628dfaae158f5ad009d84f7
davideerusso <rd.96.rd@gmail.com>
2025-04-09 17:01:09 +0200

    on/off

--- main.js
+++ main.js
@@ -8,9 +8,18 @@ console.log(lampadinaSpenta);
 console.log(button);
 
 button.addEventListener("click", () => {
-  button.classList.replace("btn-secondary", "btn-primary");
-  button.innerHTML = "Acceso";
-  lampadinaSpenta.src = "./img/yellow_lamp.png";
+  //   button.classList.replace("btn-secondary", "btn-primary");
+  //   button.innerHTML = "Acceso";
+  //   lampadinaSpenta.src = "./img/yellow_lamp.png";
+  if (button.classList.contains("btn-primary")) {
+    button.classList.replace("btn-primary", "btn-secondary");
+    button.innerHTML = "Spento";
+    lampadinaSpenta.src = "./img/white_lamp.png";
+  } else {
+    button.classList.replace("btn-secondary", "btn-primary");
+    button.innerHTML = "Acceso";
+    lampadinaSpenta.src = "./img/yellow_lamp.png";
+  }
 });
 
 console.log(this);
